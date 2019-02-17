d895ccee1287a2c405ed13bf2cb2945d01dcf5a9
A. Z. Guo <albertguotrs@hotmail.com>
2019-02-17 00:43:24 -0600

    scale up the scraping scope to more than 3695300 people

--- scrape_people_names.py
+++ scrape_people_names.py
@@ -1,39 +1,31 @@
-# from time import time
-# from warnings import warn
-#
-# from bs4 import BeautifulSoup, ResultSet
-# from requests import get
-#
-#
-# start_time = time()
-# people_names = []
-#
-# file = open("people_names.txt", encoding="utf-8", mode="w")
-# for page in range(1, 11):
-#     url = "https://www.imdb.com/list/ls058011111/?sort=list_order,asc&mode=detail&page=" + str(page)
-#     response = get(url)
-#     elapsed_time = time() - start_time
-#     print('Page {0} Request... elapsed time: {1:.2f} seconds'.format(page, elapsed_time))
-#
-#     # throw a warning for non-200 status codes
-#     if response.status_code != 200:
-#         warn('Page {0} Request... Status code: {}'.format(page, response.status_code))
-#
-#     html_soup = BeautifulSoup(response.text, 'html.parser')
-#     actor_containers: ResultSet = html_soup.find_all('div', class_='lister-item mode-detail')
-#
-#     for container in actor_containers:
-#         name = container.h3.a.text.strip()
-#         people_names.append(name)
-#         file.write(name + '\n')
-# file.closed
+from time import time
+from warnings import warn
 
-try:
-    with open('people_names.txt', 'r') as file:
-        people_names = []
-        for row in file:
-            people_names.append(row.strip())
-    file.closed
-except:
-    pass
-print(people_names[:10])
+from bs4 import BeautifulSoup, ResultSet
+from requests import get
+
+
+start_time = time()
+people_names = []
+file = open("people_names.txt", encoding="utf-8", mode="w")
+
+# scraping from IMDb
+# Birth Date between 1960-01-01 and 1993-12-31, Males/Females
+for page in range(1, 36953, 100):
+    url = "https://www.imdb.com/search/name?birth_date=1960-01-01,1993-12-31&gender=male,female&count=100&start={0}&ref_=rlm".format(page)
+    response = get(url)
+    elapsed_time = time() - start_time
+    print('scraped {0} people... elapsed time: {1:.2f} seconds'.format(page, elapsed_time))
+
+    # throw a warning for non-200 status codes
+    if response.status_code != 200:
+        warn('scraped {0} people... Status code: {}'.format(page, response.status_code))
+
+    html_soup = BeautifulSoup(response.text, 'html.parser')
+    actor_containers: ResultSet = html_soup.find_all('div', class_='lister-item mode-detail')
+
+    for container in actor_containers:
+        name = container.h3.a.text.strip()
+        people_names.append(name)
+        file.write(name + '\n')
+file.closed
